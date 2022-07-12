f7959d74bd11aa13c524ab97ac38e63afef72dd5
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-07-12 13:06:48 +0500

    React router is added and navbar is almost done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import Navbar from "./components/Navbar";
 import Container from '@mui/material/Container';
+import Layout from './components/Layout/Layout';
+import { Catalogue, Contacts, Home, Info, More, NotFound } from './pages';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
     return (
         <Container maxWidth="xl" sx={{paddingX: 0}}>
-            <Navbar />
+            <Routes>
+                <Route path='/' element={<Layout />}>
+                    <Route index element={<Home />}></Route>
+                    <Route path='catalogue' element={<Catalogue />}></Route>
+                    <Route path='info' element={<Info />}></Route>
+                    <Route path='contacts' element={<Contacts />}></Route>
+                    <Route path='more' element={<More />}></Route>
+                    <Route path='*' element={<NotFound />}></Route>
+                </Route>
+            </Routes>
         </Container>
     )
 }
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export {default as Home} from './Home';
+export {default as Catalogue} from './Catalogue';
+export {default as Contacts} from './Contacts';
+export {default as Info} from './Info';
+export {default as More} from './More';
+export {default as NotFound} from './NotFound';
